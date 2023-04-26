fa13b6f7e6b9451e4884240932d8c4d682a48102
Edward Lee <edwardneo@berkeley.edu>
2023-04-26 02:53:28 -0700

    Updates

---
+++ precache-manifest.31c8de4240d0ff98d6848764c3148178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dcf9e255304cb108cec",
+    "url": "/static/css/main.98242be4.chunk.css"
+  },
+  {
+    "revision": "0dcf9e255304cb108cec",
+    "url": "/static/js/main.58693ded.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "724cebf672e24be98600",
+    "url": "/static/js/2.46532688.chunk.js"
+  },
+  {
+    "revision": "c4ba09f844c666f5341ceef1844f7386",
+    "url": "/index.html"
+  }
+];
