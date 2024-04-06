b68aeda597496b8650d6b33a6df33501c241ad4c
BoyuFan1 <david_fan1@berkeley.edu>
2024-04-05 21:59:11 -0700

    update client/src/intro-exit/MidSurvey.jsx

--- client/src/intro-exit/MidSurvey.jsx
+++ client/src/intro-exit/MidSurvey.jsx
@@ -4,89 +4,106 @@ import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { FEEDBACK_QUESTIONS } from '../../../settings/FeedbackQuestions';
 
-// import '../css/Questionnaire.css';
+import '../css/Questionaire.css'
 import '../css/MazeGameSurvey.css';
+
 export function MidgameSurvey({ next }) {
-  const player = usePlayer();
-  const [responses, setResponses] = useState(FEEDBACK_QUESTIONS.reduce(
+	const player = usePlayer()
+  const [ feedback, setFeedback ] = useState(FEEDBACK_QUESTIONS.reduce(
     (obj, category) => ({...obj, [category.title]: category.questions.reduce(
       (obj, question) => ({...obj, [question.tag]: null}),
       {})}),
     {}
-  ));
-  const [failedSubmit, setFailedSubmit] = useState(false);
-  const topRef = useRef(null);
+  ))
+  const [ failedSubmit, setFailedSubmit ] = useState(false)
+  const topRef = useRef(null)
 
   const setAnswer = (value, category, tag, multiselect=false) => {
-    const responsesCopy = {...responses};
+    const feedbackCopy = {...feedback}
 
     if (multiselect) {
-      const target = value;
-      if (responsesCopy[category][tag] === null) {
-        responsesCopy[category][tag] = [];
+      const target = value
+      if (feedbackCopy[category][tag] === null) {
+        feedbackCopy[category][tag] = []
       }
 
       if (target.checked) {
-        responsesCopy[category][tag].push(target.value);
-        responsesCopy[category][tag].sort();
+        feedbackCopy[category][tag].push(target.value)
+        feedbackCopy[category][tag].sort()
       } else {
-        responsesCopy[category][tag] = responsesCopy[category][tag].filter(e => e !== target.value);
+        feedbackCopy[category][tag] = feedbackCopy[category][tag].filter(e => e != target.value)
       }
     } else {
-      responsesCopy[category][tag] = value;
+      feedbackCopy[category][tag] = value
     }
     
-    setResponses(responsesCopy);
-  };
+    setFeedback(feedbackCopy)
+  }
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const surveyComplete = Object.values(responses).every(category => 
-      Object.values(category).every(answer => 
-        answer !== null && (Array.isArray(answer) ? answer.length !== 0 : answer.trim() !== '')
-      )
-    );
+	const handleSubmit = event => {
+		event.preventDefault()
+    const surveyComplete = Object.values(feedback).every(category => Object.values(category).every(answer => answer !== null && (Array.isArray(answer) ? answer.length !== 0 : answer.trim() !== '')))
     if (surveyComplete) {
-      player.set('midgameSurvey', responses);
-      next();
+      player.set('midgameSurvey', feedback)
+      next()
     } else {
-      topRef.current.scrollIntoView({ behavior: "smooth" });
-      setFailedSubmit(true);
+      topRef.current.scrollIntoView({ behavior: "smooth" })
+      setFailedSubmit(true)
     }
-  };
+	}
 
   const renderQuestion = (question, category, questionNum) => {
-    return (
-      <Question
-        question={question.question}
-        type={question.type}
-        tag={question.tag}
-        num={questionNum}
-        choices={question.choices}
-        value={responses[category][question.tag]}
-        format={{ direction: question.direction }}
-        setAnswer={(value, tag) => setAnswer(value, category, tag, question.type === 'ms')}
-        key={`${question.tag}`}
-      />
-    );
-  };
+    if (question.type == 'mc' || question.type == 'likert' || question.type == 'ms') {
+      return (
+        <Question
+          question={question.question}
+          type={question.type}
+          tag={question.tag}
+          num={questionNum}
+          choices={question.choices}
+          value={feedback[category][question.tag]}
+          format={{ direction: question.direction }}
+          setAnswer={(value, tag) => setAnswer(value, category, tag, question.type == 'ms')}
+          key={`${question.tag}`}
+        />
+      )
+    } else if (question.type == 'fr') {
+      return (
+        <Question
+          question={question.question}
+          type={question.type}
+          tag={question.tag}
+          value={feedback[category][question.tag]}
+          format={{ direction: question.direction }}
+          setAnswer={(value, tag) => setAnswer(value, category, tag)}
+          key={`${question.tag}`}
+        />
+      )
+    }
+  }
 
   return (
     <div className='midgame-survey'>
-      <h1 className='midgame-survey-title' ref={topRef}>Midgame Survey</h1>
-      <p className={`midgame-survey-blurb${failedSubmit ? ' blurb-bold' : ''}`}>Please answer all the following questions to continue.</p>
-      <div className='questionnaire'>
-        {MIDGAME_QUESTIONS.map((category, catIndex) => (
-          <div key={catIndex}>
-            <h2 className='category-title'>{category.title}</h2>
-            {category.blurb ? <p>{category.blurb}</p> : null}
-            <div className='midgame-questions'>{category.questions.map((question, index) => renderQuestion(question, category.title, index))}</div>
-          </div>
-        ))}
+      <h1 className='midgame-survey-title' ref={topRef}>Exit Survey</h1>
+      <p className={`midgame-survey-blurb${ failedSubmit ? ' blurb-bold' : ''}`}>Please answer all of the following questions.</p>
+      <div className='questionaire'>
+        {
+          EXITQUESTIONS.map(category => (
+            <>
+              <h2 className='category-title' key={`${category.title}`}>{category.title}</h2>
+              {
+                'blurb' in category ?
+                <p>{category.blurb}</p> :
+                null
+              }
+              <div className='midgame-questions' key={`${category.title} questions`}>{category.questions.map((question, index) => renderQuestion(question, category.title, index))}</div>
+            </>
+          ))
+        }
         <Button className='intro-submit-btn' primary handleClick={handleSubmit}>
           <p>Submit</p>
         </Button>
       </div>
     </div>
-  );
+  )
 }
